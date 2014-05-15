8915e2f9a0bb068eefe46451018b1e7bfc54ef5d
tingtingxu <TingtingXuUs@gmail.com>
2014-05-15 15:36:41 -0400

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -4,55 +4,58 @@ public class AddTwoNumbers{
 	 * See another version of this question in AddTwoNumber.java
 	 * in which the two parameters are NOT in reversed order.
 	 */
-  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ArrayList<Integer> sum = new ArrayList<Integer>();
-		int carry = 0;
-		while(l1!=null || l2!=null){
-			if(l1!=null&&l2!=null){
-				int digitSum = l1.val + l2.val + carry;
-				if(digitSum>9){
-					carry = 1;
-					sum.add(new Integer(String.valueOf(new StringBuilder(digitSum+"").charAt(1))));
-				}else{
-					carry = 0;
-					sum.add(digitSum);
+  	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		if(l1==null&&l2==null) return null;
+		else if(l1==null) return l2;
+		else if(l2==null) return l1;
+		else{
+			ArrayList<Integer> sum = new ArrayList<Integer>();
+			int carry = 0;
+			while(l1!=null || l2!=null){
+				if(l1!=null&&l2!=null){
+					int digitSum = l1.val + l2.val + carry;
+					if(digitSum>9){
+						carry = 1;
+						sum.add(new Integer(String.valueOf(new StringBuilder(digitSum+"").charAt(1))));
+					}else{
+						carry = 0;
+						sum.add(digitSum);
+					}
+					l1 = l1.next;
+					l2 = l2.next;
+				}else if(l1!=null){
+					int digitSum = l1.val + carry;
+					if(digitSum>9){
+						carry = 1;
+						sum.add(new Integer(String.valueOf(new StringBuilder(digitSum+"").charAt(1))));
+					}else{
+						carry = 0;
+						sum.add(digitSum);
+					}
+					l1 = l1.next;
+				}else if(l2!=null){
+					int digitSum = l2.val + carry;
+					if(digitSum>9){
+						carry = 1;
+						sum.add(new Integer(String.valueOf(new StringBuilder(digitSum+"").charAt(1))));
+					}else{
+						carry = 0;
+						sum.add(digitSum);
+					}
+					l2 = l2.next;
 				}
-				l1 = l1.next;
-				l2 = l2.next;
-			}else if(l1!=null){
-				int digitSum = l1.val + carry;
-				if(digitSum>9){
-					carry = 1;
-					sum.add(new Integer(String.valueOf(new StringBuilder(digitSum+"").charAt(1))));
-				}else{
-					carry = 0;
-					sum.add(digitSum);
-				}
-				l1 = l1.next;
-			}else if(l2!=null){
-				int digitSum = l2.val + carry;
-				if(digitSum>9){
-					carry = 1;
-					sum.add(new Integer(String.valueOf(new StringBuilder(digitSum+"").charAt(1))));
-				}else{
-					carry = 0;
-					sum.add(digitSum);
-				}
-				l2 = l2.next;
 			}
-		}
-		//code handling carry for last node here
-		if(carry==1){
-			sum.add(1);
-		}
-		if(sum.size()>0){
+			//code handling carry for last node here
+			if(carry==1){
+				sum.add(1);
+			}
 			ListNode result = new ListNode(sum.get(sum.size()-1));
 			for(int index=sum.size()-2;index>=0;index--){
-				result = new ListNode(sum.get(index), result);
+				ListNode tempNode = new ListNode(sum.get(index));
+				tempNode.next = result;
+				result = tempNode;
 			}
 			return result;
-		}else{
-			return null;
 		}
 	}
 }
