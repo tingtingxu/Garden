24372a6601d81e54e561d42e09d7f4df5fbe0a6f
tingtingxu <TingtingXuUs@gmail.com>
2014-05-16 11:21:54 -0400

    Update ReverseLinkedList.java

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -1,8 +1,8 @@
-import datastructure.ListNode;
+import datastructure.ListNode;//Singly Linked List
 
 public class ReverseLinkedList{
-  //recursively solution
-  public ListNode reverseLinkedList(ListNode l1, ListNode preListNode) {
+  	//recursively solution
+  	public ListNode reverseLinkedList(ListNode l1, ListNode preListNode) {
 		if(l1==null&&preListNode==null) return l1;
 		if(l1.next==null){
 			l1.next = preListNode;
@@ -11,5 +11,19 @@ public class ReverseLinkedList{
 		ListNode nextNode = l1.next;
 		l1.next = preListNode;
 		return reverseLinkedList(nextNode, l1);
+	}
+	
+	//Non-recursivly solution.
+	public ListNode reverse(ListNode l1){
+		if(l1==null||l1.next==null) return l1;
+		ListNode preNode = l1.next;
+		l1.next = null;
+		while(preNode!=null){
+			ListNode tempNode = preNode.next;
+			preNode.next = l1;
+			l1 = preNode;
+			preNode = tempNode;
+		}
+		return l1;
 	}
 }
